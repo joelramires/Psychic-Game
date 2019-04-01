69a798f02314d9d6b826d806d077d7db1a1f0fa7
Joel Ramirez <joelramirez@MacBook-Pro-4.local>
2019-04-01 10:08:59 -0400

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// array to store alphabet choices 
+// array to store computer choices 
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 
@@ -6,46 +6,52 @@ var wins = 0;
 var losses = 0;
 var guessLeft = 9;
 var lettersAlreadyGuessed = [];
-var computerGuess = computerChoices[Math.floor(Math.random()* computerChoices.length)];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 var gameHTML = document.getElementById("game");
 var $wins = document.getElementById("wins");
 var $losses = document.getElementById("losses");
 var $userGuess = document.getElementById("userguess");
 var $computerGuess = document.getElementById("computerGuess");
-var $lettersAlreadyGuessed = document.getElementById("#lettersAlreadyGuessed");
+var $lettersAlreadyGuessed = document.getElementById("guessesSoFar");
+console.log($lettersAlreadyGuessed);
+var $guessesLeft = document.getElementById("guessesLeft");
 
-// var computerGuess random number function
-var computerGuess = computerChoices[Math.floor(Math.random()* computerChoices.length)]
-function computerGuess(computerChoices) {
-  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+// function for computer to select random letter from computer choices array
+
+
+function newGame() {
+  computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)]
+
+  guessLeft = 9;
+  lettersAlreadyGuessed.length = 0;
 }
- 
-function updatelettersAlreadyGuessed() {
-  document.getElementById("guessesSoFar").innerHTML  = "Your Guesses so far: " + lettersAlreadyGuessed.join(', ')
-}
 
-
-// fuction to reset game
-document.onkeyup = function(event){
+// fuction to reset game(not working)
+document.onkeyup = function (event) {
   var userGuess = event.key;
-  if (guessLeft > 0){
-   if(userGuess == computerGuess){
-    wins++;
-    document.getElementById("wins").innerHTML = "Wins: " + wins;
-    newGame()
-  }
-      else {
-        guessLeft--;
-        document.getElementById("guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
+  if (guessLeft > 0) {
+    lettersAlreadyGuessed.push(userGuess)
 
-      }
-  if(guessLeft == 0){
-    loss++;
-    document.getElementById("losses").innerHTML = "Losses: " + losses;
-    newgame()
+    if (userGuess == computerGuess) {
+      wins++;
+      newGame()
+    } else {
+      guessLeft--;
+
+    }
+    if (guessLeft == 0) {
+      losses++;
+      newGame()
+    }
   }
-  }
+  $wins.innerHTML = `${wins}`;
+  $losses.innerHTML = `${losses}`;
+  $lettersAlreadyGuessed.innerHTML = `${lettersAlreadyGuessed}`;
+  $userGuess.innerHTML = `${userGuess}`;
+  $guessesLeft.innerHTML = guessLeft;
 }
+
 // run newGame() to start game for the first time 
 
+newGame();
